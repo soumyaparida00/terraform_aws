ae37c9fef35d9ccdd0bfa640d0e4d2709d847b7d
soumyaparida00 <soumyaparida00@gmail.com>
2022-11-01 00:36:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+// Provider configuration
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "s3-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
