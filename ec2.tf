63164439ea6ea90f6da31ce8b8a4773749872b6b
soumyaparida00 <soumyaparida00@gmail.com>
2022-12-01 09:01:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "instance_name" {
     # Replace with ami-id with the ami id needs to be used
-    ami = "ami-id"
+    ami = "ami-08c40ec9ead489470"
     instance_type = "t2.micro"
     # VPC
-    subnet_id = "${aws_subnet.your_subnet_name_public-1a.id}"
+    subnet_id = "${aws_subnet.vtb-sub-pub-1a.id}"
     # Security Group
     vpc_security_group_ids = [
         "${aws_security_group.openvpn.id}",
@@ -19,9 +19,7 @@ resource "aws_instance" "instance_name" {
         timeout     = "4m"
     }
     tags = {
-        Name = "instance_name"
-        Env = "your_env_name"
-        Project = "your_project_name"
+        Name = "test_server"
     }
 }
 // Sends your public key to the instance
@@ -30,42 +28,6 @@ resource "aws_key_pair" "id_rsa" {
     public_key = file("Path of local key file which needs to be pushed to instance")
 
     tags = {
-        Name = "instance_name"
-        Env = "your_env_name"
-        Project = "your_project_name"
-    }
-}
-resource "aws_instance" "jenkins_instance_name" {
-    # Replace with ami-id with the ami id needs to be used
-    ami = "ami-id"
-    instance_type = "t2.micro"
-    # VPC
-    subnet_id = "${aws_subnet.your_subnet_name_private-1a.id}"
-    # Security Group
-    vpc_security_group_ids = "${aws_security_group.ssh-allowed.id}"
-    # the Public SSH key
-    key_name = "id_rsa"
-    connection {
-        type        = "ssh"
-        host        = self.public_ip
-        user        = "ubuntu"
-        private_key = file("Path of local key file which needs to pushed to instance")
-        timeout     = "4m"
-    }
-    tags = {
-        Name = "jenkins_instance_name"
-        Env = "your_env_name"
-        Project = "your_project_name"
-    }
-}
-// Sends your public key to the instance
-resource "aws_key_pair" "id_rsa" {
-    key_name = "id_rsa"
-    public_key = file("Path of local key file which needs to be pushed to instance")
-
-    tags = {
-        Name = "jenkins_instance_name"
-        Env = "your_env_name"
-        Project = "your_project_name"
+        Name = "test_server"
     }
 }
