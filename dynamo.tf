d2f08390748a58f2a75b740cfaa9319b07773bc5
soumyaparida00 <soumyaparida00@gmail.com>
2022-12-01 09:02:35 +0530

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = "vtb-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
