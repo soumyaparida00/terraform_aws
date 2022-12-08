58c063ca6cf14c2c57be1b92343c98e9b33a8b8e
Lipsa Das <soumyaparida00@gmail.com>
2022-12-08 08:43:55 +0530

    some random changes

--- main.tf
+++ main.tf
@@ -5,12 +5,16 @@ terraform {
      source  = "hashicorp/aws"
      version = "~> 3.0"
    }
+   kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
  }
 backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-vtb"
+    bucket         = "s3-bucket"
     key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "vtb-terraform-locks"
     encrypt        = true
