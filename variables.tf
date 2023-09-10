84456e0b3df13284192abd46ccf4a1e131458f33
soumyaparida00 <soumyaparida00@gmail.com>
2023-09-10 18:11:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
 variable "AWS_REGION" {    
     default = "ap-south-1"
 }
+variable "clustername"{
+    default = "test-eks"
+    description = "Test  EKS Cluster"
+}
+variable "spot_instance_types"{
+    default = ["t3a.small","t3a.medium"]
+    description = "List of instance types for SPOT instance selection"
+}
+variable "spot_max_size"{
+    default = 2
+    description = "How many SPOT instance can be created max"
+}
+variable "spot_desired_size"{
+    default = 4
+    description = "How many SPOT instance should be running at all times"
+}
