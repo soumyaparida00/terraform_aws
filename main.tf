c7ad50f9604b49d9d4907ba5decd53604d5bdd1c
soumyaparida00 <soumyaparida00@gmail.com>
2023-09-10 14:26:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ backend "s3" {
     key            = "global/s3/terraform.tfstate"
     region         = "ap-south-1"
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
+    #dynamodb_table = "terraform-up-and-running-locks"
+    #encrypt        = true
   }
 }
